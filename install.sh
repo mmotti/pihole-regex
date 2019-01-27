763cf8f9ff5342e11c3796131d8ee6549a038a97
MMotti <matthew.w.motti@gmail.com>
2019-01-27 13:52:18 +0000

    Slightly improve install process

--- install.sh
+++ install.sh
@@ -1,42 +1,55 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1117
 
-# Get existing Pi-Hole style wildcards
-# (^|\.)test\.com$
-echo "--> Identifying Pi-hole wildcards"
-pihole_wildcards="$(grep "^(\^|.*\$$" /etc/pihole/regex.list)"
+# Set regex outputs
+file_pihole_regex="/etc/pihole/regex.list"
+file_mmotti_regex="/etc/pihole/mmotti-regex.list"
 
-# Fetch mmotti regex.list
-echo "--> Fetching mmotti's regex.list"
-mmotti_regex=$(wget -qO - https://raw.githubusercontent.com/mmotti/pihole-regex/master/regex.list)
-
-# Exit if unable to download list
-if [ -z "$mmotti_regex" ]; then
-	echo "Error: Unable to fetch mmotti regex.list"
-	exit
-else
-	echo "--> $(wc -l <<< "$mmotti_regex") regexps found in mmotti's regex.list"
+# Restore config prior to previous install
+# Keep entries only unique to pihole regex
+if [ -s "$file_pihole_regex" ] && [ -s "$file_mmotti_regex" ]; then
+	echo "[i] Removing mmotti's regex.list from a previous install"
+	comm -23 <(sort $file_pihole_regex) <(sort $file_mmotti_regex) | sudo tee $file_pihole_regex > /dev/null
 fi
 
-# Form output (preserving Pi-hole wildcards)
-if [ -z "$pihole_wildcards" ]; then
-	echo "--> No Pi-hole specific wildcards detected"
-	final_regex=$(printf "%s\n" "$mmotti_regex")
+# Fetch mmotti regex.list
+echo "[i] Fetching mmotti's regex.list"
+sudo wget -qO "$file_mmotti_regex" https://raw.githubusercontent.com/mmotti/pihole-regex/master/regex.list
+
+# Exit if unable to download list
+if [ ! -s "$file_mmotti_regex" ]; then
+        echo "Error: Unable to fetch mmotti regex.list"
+        exit
 else
-	echo "--> $(wc -l <<< "$pihole_wildcards") Pi-hole specific wildcards detected"
-	final_regex=$(printf "%s\n" "$mmotti_regex" "$pihole_wildcards")
+        mmotti_regex="$(cat $file_mmotti_regex)"
+        echo "[i] $(wc -l <<< "$mmotti_regex") regexps found in mmotti's regex.list"
+fi
+
+# Check existing configuration
+if [ -s "$file_pihole_regex" ]; then
+	# Extract non mmotti-regex entries
+	existing_regex_list="$(cat $file_pihole_regex)"
+
+	# Form output (preserving existing config)
+        echo "[i] $(wc -l <<< "$existing_regex_list") regexps exist outside of mmotti's regex.list"
+        final_regex=$(printf "%s\n" "$mmotti_regex" "$existing_regex_list")
+
+else
+	echo "[i] No regex.list differences to mmotti's regex.list"
+	final_regex=$(printf "%s\n" "$mmotti_regex")
+
 fi
 
 # Output to regex.list
-echo "--> Saving to /etc/pihole/regex.list"
-LC_COLLATE=C sort -u <<< "$final_regex" | sudo tee /etc/pihole/regex.list > /dev/null
+echo "[i] Saving to $file_pihole_regex"
+LC_COLLATE=C sort -u <<< "$final_regex" | sudo tee $file_pihole_regex > /dev/null
 
 # Refresh Pi-hole
-echo "--> Refreshing Pi-hole"
+echo "[i] Refreshing Pi-hole"
 sudo killall -SIGHUP pihole-FTL
 
-echo "--> Done"
+echo "[i] Done"
 
 # Output to user
 echo $'\n'
-cat /etc/pihole/regex.list
+cat $file_pihole_regex
