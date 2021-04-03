4e19a2c5f3f801d287cf7c64592aaf7032db1bbc
Clayton Auld <clayauld@gmail.com>
2021-04-02 22:11:27 -0800

    Added quick whitelist install script.

---
+++ install_whitelist.sh
@@ -0,0 +1,13 @@
+# User reported false positives for MMotti's regex filters
+# https://github.com/mmotti
+
+# This file is not currently referenced during install.
+# If you experience issues with any of the following domains,
+# you will need the add them manually to your Pi-hole installation.
+pihole -w \
+anti-ad.net \
+iij.ad.jp \
+stats.foldingathome.org \
+stats.stackexchange.com \
+www.ad.nl \
+www.iij.ad.jp
